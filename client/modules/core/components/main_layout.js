2bff69227e4c2d644f239149b66268d7c51e8ec2
Weiming Zhai <wmzhai@gmail.com>
2016-04-04 12:28:02 +0800

    apply app-layout

--- client/modules/core/components/main_layout.js
+++ client/modules/core/components/main_layout.js
@@ -4,12 +4,13 @@ import Header from './header';
 const Layout = ({content = () => null }) => (
   <div>
     <Header />
-    <div>
-    {content()}
-    </div>
 
-    <footer>
-    <small>Built with <a href='https://github.com/kadirahq/mantra'>Mantra</a> &amp; Meteor.</small>
+    <main className="app-layout">
+      {content()}
+    </main>
+
+    <footer className="site-footer">
+      <small>基于<a href='https://github.com/kadirahq/mantra'>Mantra</a> &amp; Meteor构建.</small>
     </footer>
   </div>
 );
